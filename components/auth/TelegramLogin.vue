f072cf720ead965b76b9199f63a69f467494fb93
Dalin <139043312+ladalin23@users.noreply.github.com>
2026-01-12 01:35:38 +0700

    Update TelegramLogin.vue

--- components/auth/TelegramLogin.vue
+++ components/auth/TelegramLogin.vue
@@ -28,7 +28,8 @@ window.onTelegramAuth = async (user) => {
   const profile_picture = user.photo_url || "";
   console.log("Profile picture URL:", profile_picture);
   try {
-    await userAuthStore.login(username, telegram_id, profile_picture);
+    const authSrr = await userAuthStore.login(username, telegram_id, profile_picture);
+    console.log("Auth Store is:", authSrr)
   } catch (error) {
     console.error("Login error:", error);
     await $swal.fire({
