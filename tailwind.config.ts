17b2c91094d73c8dfba247df3dbb4359e117329d
Dalin <139043312+ladalin23@users.noreply.github.com>
2026-01-13 15:48:51 +0700

    up

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class", // This is the crucial line
+  content: [
+    
+  ],
+  // ... rest of your config
+};
+export default config;
