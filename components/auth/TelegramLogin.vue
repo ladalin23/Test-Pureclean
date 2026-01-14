64e36842d477c7a1558196dd718f55f0a81909ae
Dalin <139043312+ladalin23@users.noreply.github.com>
2026-01-14 15:33:58 +0700

    Update TelegramLogin.vue

--- components/auth/TelegramLogin.vue
+++ components/auth/TelegramLogin.vue
@@ -14,7 +14,7 @@ onMounted(() => {
   const script = document.createElement("script");
   script.src = "https://telegram.org/js/telegram-widget.js?22";
   script.setAttribute("data-telegram-login", botUsername);
-  script.setAttribute("data-size", "large");
+  script.setAttribute("data-size", "medium");
   script.setAttribute("data-radius", "20");
   script.setAttribute("data-userpic", "false");
   script.setAttribute("data-onauth", "onTelegramAuth(user)");
